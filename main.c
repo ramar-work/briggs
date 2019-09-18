ecc6ff677ee249e10fda480361296ad30c1f41c9
Antonio R. Collins II <ramar.collins@gmail.com>
2019-09-17 20:41:32 -0400

    Trying something else.

--- main.c
+++ main.c
@@ -32,64 +32,25 @@ typedef enum {
 , STREAM_XML
 , STREAM_SQL
 , STREAM_CSTRUCT
+, STREAM_COMMA
+, STREAM_CARRAY
+, STREAM_JCLASS
+, STREAM_CUSTOM
 
 } Stream;
 
 
-//Blog content text
-//#include "test/etc.c"
-
 //Global variables to ease testing
-char titleBuf[ 1024 ] = { 0 };  /*Titles are no more than 1024 characters*/
-int rows              =    10;  /*Assume we want 100 rows in the database*/
-
 char *prefix = NULL;
 char *suffix = NULL;
-
-//Resource list
-static const struct Resources { const char type, *path; } 
-resources[] = {
-	{ 'd', "models" },
-	{ 'd', "views"  },
-	{ 'd', "sql"    },
-	{ 'd', "static" },
-	{ 'd', "vendor" },
-	{ 'f', "index.php" },
-	{ 'f', "routes.json" },
-};
-
-
-//Load in assets from different places online (pure.css, jquery, etc)
-#if 0
-static const struct Assets { const char *name, *url; } 
-assets[] = {
-	{ "pure"  , "https://github.com/yahoo/pure-release/archive/v0.6.2.zip" },
-	{ "jquery", "https://code.jquery.com/jquery-3.1.1.min.js" }
-};
-#endif
-
-
-
-
-//Some stuff to read from a file...
-#if 0
-int readme() 
-{
-	struct stat sb;
-	int fd;
-	if ( stat( file, &sb ) == -1 )
-		return nerr( "failed to stat file: %s\n", file );
-	
-	if ((fd = open( file, O_RDONLY )) == -1 )
-		return nerr( "failed to open %s\n", file );
-
-	if (read( fd, buf, sb.st_size ) == -1 )
-		return nerr( "failed to read all of file %s\n", file );
-
-	if ( close ( fd ) == -1 )
-		return nerr( "failed to close file %s\n", file );
-}
-#endif
+char *stream_chars = NULL;
+char *sqltable = NULL;
+int newline=0;
+int hlen;
+int adv=0;
+static const char *ucases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+static const char *lcases = "abcdefghijklmnopqrstuvwxyz_";
+//const char *exchrs = "~`!@#$%^&*()-_+={}[]|:;"'<>,.?/";
 
 
 //rand number between;
@@ -99,212 +60,35 @@ long lbetween (long min, long max) {
 	return r;
 }
 
-
-
-//generate a randomly long title name
-char *title (char *buf, int maxBuff, int words) {
-	int p = 0, 
-			w = !words ? 1 : words;
-
-	for ( int ii=0; (p + 1) > maxBuff || ii<w; ii++ )
-		p += sprintf( &buf[ p ], "%s ", titles[rand_number() % (sizeof(titles) / sizeof(char *))]);
-
-	buf[ p - 1 ] = '\0';
-	return buf;
-}
-
-
-
-//Quick random generate
-int seeddb (void) 
-{
-	struct timespec tspec;
-	clock_gettime( CLOCK_REALTIME, &tspec ); 
-	int authorMax  = sizeof( authors ) / sizeof(char *);
-	int titleMax   = sizeof( titles ) / sizeof(char *);
-	int contentMax = sizeof( content ) / sizeof(char *) ;
-	FILE *out      = stdout;
-
-	const int       dMin = 915166800;                 /*Jan 01, 1999*/
-	const int       dMax = tspec.tv_sec;              /*Jan 01, 1999*/
-	const char *iAuthors = "INSERT INTO authors VALUES "
-                         "(NULL, '%s', '%s', '%s', %d, %d);\n";
-	const char *iPosts   = "INSERT INTO posts VALUES "
-		                     "( NULL, '%s', %d, %d, %d );\n"; 
-	const char *iContent = "INSERT INTO content VALUES "
-                         "(NULL, %d, %d, '%s', %d, %d);\n";
-
-	Parser p = { .words = {{"\n"},{NULL}}};
-	pr_prepare( &p );
-
-	//Write the load statement 
-	fprintf( out, "%s", loadSql );
-
-	//Add all these authors just because
-	for (int i=0 ; i < authorMax; i++ ) 
-		fprintf( out, iAuthors, authors[i], authorIDs[i], pwds[i],
-			lbetween( dMin, dMax ), lbetween( dMin, dMax ));
-
-	//Seed the post database
-	for (int i=0; i < rows; i++ )
-	{
-		//Decide on a post name, author and what not
-		int aLimit  = rand_number() % authorMax; //(int)lbetween( 1, authorMax );
-		int cLimit  = rand_number() % contentMax; //(int)lbetween( 1, contentMax );
-		long dAdded = lbetween( dMin, dMax - 3600 ); 
-		long dMod   = lbetween( dAdded, dMax - 3600 ); 
-		char *t     = title( titleBuf, sizeof(titleBuf), rand_number() % 6 );
-
-		//Output the post add statement
-		fprintf( out, iPosts, t, aLimit, dAdded, dMod );
-		
-		//Then generate the content inserts.
-		for ( int ii=cLimit, cid=0; ii < sizeof(content)/sizeof(char *); ii++, cid++ ) 
-		{
-			long dAdded = lbetween( dMin, dMax - 3600 ); 
-			long dMod   = lbetween( dAdded, dMax - 3600 ); 
-			fprintf( out, iContent, i, cid, content[ii], dAdded, dMod ); 
+//Convert word
+//char *w = strdup( "WORD" );snakecase( &w );
+void snakecase ( char **k ) {
+	char *nk = *k;
+	int p=0;
+	while ( *nk ) {
+		char *u = (char *)ucases, *l = (char *)lcases;
+		while ( *u ) {
+			if ( *nk == *u ) {
+				*nk = *l;
+				break;
+			}
+			u++, l++;
 		}
+		nk++;
 	}
-
-	return 1;	
 }
 
-
-int json (void)
-{
-	struct timespec tspec;
-	clock_gettime( CLOCK_REALTIME, &tspec ); 
-	int authorMax  = sizeof( authors ) / sizeof(char *);
-	int titleMax   = sizeof( titles ) / sizeof(char *);
-	int contentMax = sizeof( content ) / sizeof(char *) ;
-	FILE *out      = stdout;
-
-	const int       dMin = 915166800;                 /*Jan 01, 1999*/
-	const int       dMax = tspec.tv_sec;              /*Jan 01, 1999*/
-	const char *iAuthors = "\"author\": { \"%s\", \"%s\", \"%s\", %d, %d },\n";
-	const char *iPosts   = "\t\"post\": { \"title\": \"%s\", \"index\": \"%s\", \"count\": %d, \"dateAdded\": %d, \"dateModified\": %d },\n";
-	const char *iContent = "\t\"content\": { %d, \"%d\", \"%s\", %d, %d },\n";
-	const char *iRandArr = "\t\"claus\": [ \"%s\", %d, %d, %d ],\n";
-                         
-	Parser p = { .words = {{"\n"},{NULL}}};
-	pr_prepare( &p );
-
-#if 0
-	//Add all these authors just because
-	for (int i=0 ; i < authorMax; i++ ) 
-		fprintf( out, iAuthors, authors[i], authorIDs[i], pwds[i],
-			lbetween( dMin, dMax ), lbetween( dMin, dMax ));
-#endif
-	//Seed the post database
-	for (int i=0; i < rows; i++ )
-	{
-		fprintf( out, "{\n" );
-		//Decide on a post name, author and what not
-		int aLimit  = rand_number() % authorMax; //(int)lbetween( 1, authorMax );
-		int cLimit  = rand_number() % contentMax; //(int)lbetween( 1, contentMax );
-		long dAdded = lbetween( dMin, dMax - 3600 ); 
-		long dMod   = lbetween( dAdded, dMax - 3600 ); 
-		char *t     = title( titleBuf, sizeof(titleBuf), rand_number() % 6 );
-
-		
-		//Then generate the content inserts.
-		for ( int ii=cLimit, cid=0; ii < sizeof(content)/sizeof(char *); ii++, cid++ ) 
-		{
-			long dAdded = lbetween( dMin, dMax - 3600 ); 
-			long dMod   = lbetween( dAdded, dMax - 3600 ); 
-		fprintf( out, iPosts, t, rand_chars(12), aLimit, dAdded, dMod );
-			fprintf( out, iContent, i, cid, content[ii], dAdded, dMod ); 
-			fprintf( out, iRandArr, rand_chars(10), rand_number(), rand_number(), rand_number() );
-		}
-		fprintf( out, "},\n" );
-	}
-
-	return 1;
-}
-
-
-int xml (void)
-{
-	struct timespec tspec;
-	clock_gettime( CLOCK_REALTIME, &tspec ); 
-	int authorMax  = sizeof( authors ) / sizeof(char *);
-	int titleMax   = sizeof( titles ) / sizeof(char *);
-	int contentMax = sizeof( content ) / sizeof(char *) ;
-	FILE *out      = stdout;
-
-	const int       dMin = 915166800;                 /*Jan 01, 1999*/
-	const int       dMax = tspec.tv_sec;              /*Jan 01, 1999*/
-	const char *iAuthors = "\"author\": { \"%s\", \"%s\", \"%s\", %d, %d },\n";
-	const char *iPosts   = "\t\"post\": { \"title\": \"index\": \"%s\", \"count\": %d, \"dateAdded\": %d, \"dateModified\": %d },\n";
-	const char *iContent = "\t\"content\": { %d, \"%d\", \"%s\", %d, %d },\n";
-	const char *iRandArr = "\t\"claus\": [ \"%s\", %d, %d, %d ],\n";
-                         
-	Parser p = { .words = {{"\n"},{NULL}}};
-	pr_prepare( &p );
-
-#if 0
-	//Add all these authors just because
-	for (int i=0 ; i < authorMax; i++ ) 
-		fprintf( out, iAuthors, authors[i], authorIDs[i], pwds[i],
-			lbetween( dMin, dMax ), lbetween( dMin, dMax ));
-#endif
-	//Seed the post database
-	for (int i=0; i < rows; i++ )
-	{
-		fprintf( out, "{\n" );
-		//Decide on a post name, author and what not
-		int aLimit  = rand_number() % authorMax; //(int)lbetween( 1, authorMax );
-		int cLimit  = rand_number() % contentMax; //(int)lbetween( 1, contentMax );
-		long dAdded = lbetween( dMin, dMax - 3600 ); 
-		long dMod   = lbetween( dAdded, dMax - 3600 ); 
-		char *t     = title( titleBuf, sizeof(titleBuf), rand_number() % 6 );
-
-		//Output the post add statement
-		fprintf( out, iPosts, t, aLimit, dAdded, dMod );
-		
-		//Then generate the content inserts.
-		for ( int ii=cLimit, cid=0; ii < sizeof(content)/sizeof(char *); ii++, cid++ ) 
-		{
-			long dAdded = lbetween( dMin, dMax - 3600 ); 
-			long dMod   = lbetween( dAdded, dMax - 3600 ); 
-			fprintf( out, iContent, i, cid, content[ii], dAdded, dMod ); 
-			fprintf( out, iRandArr, rand_chars(10), rand_number(), rand_number(), rand_number() );
-		}
-		fprintf( out, "},\n" );
-	}
-
-	return 1;
-}
-
-
-struct Ghost 
-{
-	/*Something to print out before the loop begins*/
-	const char *pre;
-	/*Start and end encapsulators for each loop*/
-	const char *start, *end;
-	const char *blocks[10];
-};
-
-const char *blocks[] = {
-	"\"author\": { \"%s\", \"%s\", \"%s\", %d, %d },\n",
-	"\t\"post\": { \"title\": \"%s\", \"index\": \"%s\", \"count\": %d, \"dateAdded\": %d, \"dateModified\": %d },\n",
-	"\t\"content\": { %d, \"%d\", \"%s\", %d, %d },\n",
-	"\t\"claus\": [ \"%s\", %d, %d, %d ],\n"
-};
-
-
+//TODO: Bitshift the first int.  
+//control printing comma, control tabbing, control pretty formatting etc
 //Simple key-value
 void p_default( int ind, const char *k, const char *v ) {
 	fprintf( stdout, "%s => %s\n", k, v );
 }  
 
-
 //JSON converter
 void p_json( int ind, const char *k, const char *v ) {
 	//Check if v is a number, null or some other value that should NOT be escaped
-	fprintf( stdout, "%s\"%s\": \"%s\",\n", &TAB[9-ind], k, v );
+	fprintf( stdout, ",%s\"%s\": \"%s\"\n", &TAB[9-ind], k, v );
 }  
 
 //XML converter
@@ -315,13 +99,48 @@ void p_xml( int ind, const char *k, const char *v ) {
 //SQL converter
 void p_sql( int ind, const char *k, const char *v ) {
 	//Check if v is a number, null or some other value that should NOT be escaped
-	fprintf( stdout, "'%s', ", v );
+	fprintf( stdout, ",'%s'", v );
 }  
+
+//Simple comma converter
+void p_comma ( int ind, const char *k, const char *v ) {
+	//Check if v is a number, null or some other value that should NOT be escaped
+	fprintf( stdout, " ,\"%s\"", v );
+}
 
 //C struct converter
 void p_cstruct ( int ind, const char *k, const char *v ) {
-
+	//check that it's a number
+	char *vv = (char *)v;
+	if ( !strlen( vv ) ) {
+		fprintf( stdout, "%s, .%s = \"\"\n", &TAB[9-ind], k );
+		return;
+	}
+	#if 0
+	if ( !strcmp( vv, "true" ) || !strcmp( vv, "TRUE" ) ) {
+		fprintf( stdout, "%s.%s = 1,\n", &TAB[9-ind], k );
+		return;
+	}
+		
+	if ( !strcmp( vv, "false" ) || !strcmp( vv, "FALSE" ) ) {
+		fprintf( stdout, "%s.%s = 0,\n", &TAB[9-ind], k );
+		return;
+	}
+	#endif
+	while ( *vv ) {
+		if ( !memchr( "0123456789", *vv, 10 ) ) {
+			fprintf( stdout, ",%s.%s = \"%s\"\n", &TAB[9-ind], k, v );
+			return;	
+		}
+		vv++;
+	}
+	fprintf( stdout, "%s, .%s = %s\n", &TAB[9-ind], k, v );
 }  
+
+//C struct converter
+void p_carray ( int ind, const char *k, const char *v ) {
+	fprintf( stdout, ",\"%s\"", v );
+}
 
 
 //convert CSV or similar to another format
@@ -356,7 +175,6 @@ int convert_f ( const char *file, const char *delim, Stream stream ) {
 		fprintf(stderr,"fail...\n"); return 0;	
 	}
 
-
 	//char add, headers can be in their own, the other should be somewhere else...
 	char **headers = NULL;	
 	//char **values[];
@@ -378,12 +196,14 @@ int convert_f ( const char *file, const char *delim, Stream stream ) {
 		}
 		else if ( p.chr == ';' ) {
 			if ( p.size == 0 ) { 
+				//
 				ADD_ELEMENT( headers, hlen, char *, strdup( "nothing" ) );
 			}
 			else {
 				char *a = malloc( p.size + 1 );
 				memset( a, 0, p.size + 1 );	
 				memcpy( a, &buf[p.pos], p.size );
+				snakecase( &a );
 				ADD_ELEMENT( headers, hlen, char *, a );
 			}
 		}
@@ -408,7 +228,6 @@ exit(0);
 
 	//Theoretically, you don't need to rewind anything...
 	while ( strwalk( &p, buf, del ) ) {
-		
 		if ( p.chr == '\n' ) {
 			if ( iv || boink ) {
 				ADD_ELEMENT( iv, idvlen, Dub *, NULL );
@@ -436,8 +255,8 @@ exit(0);
 		}
 	}
 
-#if 1
 	//move through the thing
+	FILE *output = stdout;
 	ADD_ELEMENT( ov, odvlen, Dub **, NULL );	
 	while ( *ov ) {
 		Dub **bv = (Dub **)*ov;
@@ -445,49 +264,76 @@ exit(0);
 
 		//Prefix
 		if ( prefix ) {
-			fprintf( stderr, "%s\n", prefix );
+			fprintf( output, "%s", prefix );
 		}
 
 		if ( stream == STREAM_PRINTF )
 			0;//p_default( 0, ib->k, ib->v );
 		else if ( stream == STREAM_XML )
 			0;//p_xml( 0, ib->k, ib->v );
+		else if ( stream == STREAM_COMMA )
+			0;//fprintf( output, "" );
+		else if ( stream == STREAM_CSTRUCT )
+			fprintf( output, "{\n" );
+		else if ( stream == STREAM_CARRAY )
+			fprintf( output, "{ " );
+		else if ( stream == STREAM_SQL )
+			fprintf( output, "INSERT INTO %s VALUES ( ", sqltable );
 		else if ( stream == STREAM_JSON ) {
-			fprintf( stdout, "{\n" );
+			fprintf( output, "{\n" );
 		}
 
 		//The body
 		while ( *bv && (*bv)->k ) {
 			Dub *ib = *bv;
+			//Sadly, you'll have to use a buffer...
+			//Or, use hlen, since that should contain a count of columns...
 			if ( stream == STREAM_PRINTF )
 				p_default( 0, ib->k, ib->v );
 			else if ( stream == STREAM_XML )
 				p_xml( 0, ib->k, ib->v );
+			else if ( stream == STREAM_CSTRUCT )
+				p_cstruct( 1, ib->k, ib->v );
+			else if ( stream == STREAM_COMMA )
+				p_comma( 1, ib->k, ib->v );
+			else if ( stream == STREAM_SQL )
+				p_sql( 0, ib->k, ib->v );
 			else if ( stream == STREAM_JSON ) {
 				p_json( 0, ib->k, ib->v );
 			}
-			bv++;
+			adv = 1, bv++;
 		}
 
-		//Suffix
+		//Built-in suffix...
 		if ( stream == STREAM_PRINTF )
 			0;//p_default( 0, ib->k, ib->v );
 		else if ( stream == STREAM_XML )
 			0;//p_xml( 0, ib->k, ib->v );
+		else if ( stream == STREAM_COMMA )
+			0;//fprintf( output, " },\n" );
+		else if ( stream == STREAM_CSTRUCT )
+			fprintf( output, "\n}," );
+		else if ( stream == STREAM_CARRAY )
+			fprintf( output, " },\n" );
+		else if ( stream == STREAM_SQL )
+			fprintf( output, " );\n" );
 		else if ( stream == STREAM_JSON ) {
 			//wipe the last ','
-			fprintf( stdout, "},\n" );
-			//fprintf( stdout, "," );
+			fprintf( output, "},\n" );
+			//fprintf( output, "," );
 		}
 
 		//Suffix
 		if ( suffix ) {
-			fprintf( stderr, "%s\n", suffix );
+			fprintf( output, "%s", suffix );
 		}
 
+		if ( newline ) {
+			fprintf( output, "\n" );
+		}
+			
 		ov++;
 	}
-#endif
 
 	return 1;
 
@@ -514,12 +360,19 @@ Option opts[] = {
 	//Serialization formats
 	{ "-j", "--json",       "Convert into JSON." },
 	{ "-x", "--xml",        "Convert into XML." },
-	//{ "-q", "--sql",        "Generate some random XML." },
+	{ NULL, "--comma",      "Convert into XML." },
+	{ NULL, "--carray",     "Convert into a C-style array." },
+	{ NULL, "--cstruct",    "Convert into a C struct." },
+	{ "-q", "--sql",        "Generate some random XML.", 's' },
+	{ "-n", "--newline",    "Generate newline after each row." },
+	//{ NULL, "--java-class", "Convert into a Java class." },
 	//{ "-m", "--msgpack",    "Generate some random msgpack." },
 
 	//Add prefix and suffix to each row
 	{ "-p", "--prefix",     "Specify a prefix", 's' },
 	{ "-s", "--suffix",     "Specify a suffix", 's' },
+//	{ NULL, "--prefix-value",     "Specify a prefix", 's' },
+//	{ NULL, "--suffix-value",     "Specify a suffix", 's' },
 	//{ "-b", "--blank",      "Define a default header for blank lines.", 's' },
 	//{ NULL, "--no-blank",   "Do not show blank values.", 's' },
 
@@ -529,14 +382,12 @@ Option opts[] = {
 
 
 
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
 	SHOW_COMPILE_DATE();
 
 	(argc < 2) ? opt_usage(opts, argv[0], "nothing to do.", 0) : opt_eval(opts, argc, argv);
 
 	int stream_fmt = 0;
-
 #if 0
 	if ( opt_set( opts, "--rows" ) )
 		rows = opt_get(opts, "--rows").n;	
@@ -548,20 +399,53 @@ int main (int argc, char *argv[])
 		json(); 
 #endif
 
+	//Should we be type strict or not?
+	if ( opt_set( opts, "--strict" ) )
+		0;//stream_fmt = STREAM_JSON;	
+
 	if ( opt_set( opts, "--json" ) )
 		stream_fmt = STREAM_JSON;	
 
 	if ( opt_set( opts, "--xml" ) )
 		stream_fmt = STREAM_XML;	
 
+	if ( opt_set( opts, "--cstruct" ) )
+		stream_fmt = STREAM_CSTRUCT;	
+
+	if ( opt_set( opts, "--comma" ) )
+		stream_fmt = STREAM_COMMA;	
+
+	if ( opt_set( opts, "--carray" ) )
+		stream_fmt = STREAM_CARRAY;	
+
+	if ( opt_set( opts, "--newline" ) ) {
+		//consider being able to take numbers for newlines...
+		newline = 1;
+	}
+
+	if ( opt_set( opts, "--custom" ) ) {
+		stream_fmt = STREAM_CUSTOM;	
+		//stream_chars = opt_get( opts, "--custom" ).s;	
+	}
+
+	if ( opt_set( opts, "--sql" ) ) {
+		stream_fmt = STREAM_SQL;
+		sqltable = opt_get( opts, "--sql" ).s;
+		if ( !sqltable ) {
+			fprintf( stderr, "No SQL table specified for this statement.\n" );
+			exit( 1 );
+		}		
+	}
+
+	//
 	if ( opt_set( opts, "--prefix" ) ) {
 		prefix = opt_get( opts, "--prefix" ).s;	
-		fprintf( stderr, "%s\n", prefix );
+		//fprintf( stderr, "%s\n", prefix );
 	}
 
 	if ( opt_set( opts, "--suffix" ) ) {
 		suffix = opt_get( opts, "--suffix" ).s;	
-		fprintf( stderr, "%s\n", suffix );
+		//fprintf( stderr, "%s\n", suffix );
 	}
 
 	if ( opt_set( opts, "--convert" ) ) {
